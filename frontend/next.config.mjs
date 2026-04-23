406bca61053ea14a6e255d94bcac43faee375cf2
wan-elmus <elmuswangudi@gmail.com>
2026-04-23 18:21:58 +0300

    Frontend setup

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: [],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        destination: `${process.env.BACKEND_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;

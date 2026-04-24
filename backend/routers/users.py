cb4138aa2d00eceb2da74339623bd6fd92a9b7e3
wan-elmus <elmuswangudi@gmail.com>
2026-04-24 14:52:51 +0300

    updated profile endpoint and added models

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -1,6 +1,4 @@
-from typing import List
-
-from pyparsing import Optional
+from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
